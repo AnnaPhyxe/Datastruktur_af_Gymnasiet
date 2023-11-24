100007b70ac4170af5c54a551b05739eebd62185
Anna Bechmann <annabechmann@outlook.dk>
2023-11-24 08:48:44 +0100

    Remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/annab/Downloads/Datastruktur/Datastruktur
-# Build directory: C:/Users/annab/Downloads/Datastruktur/Datastruktur/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
